a5f7fc40ef00eb0874a5eb34f8ea5a56659e6bec
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 20:23:48 +0000

    Função recursiva para simular o movimento da Torre

---
+++ 03MESTRE/mestredesafio.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Função recursiva para simular o movimento da Torre
+
+void torreMovimento(int movimento) {
+    if (movimento > 0) {
+        printf("Direita\n");
+        torreMovimento(movimento - 1);
+    }
+}
