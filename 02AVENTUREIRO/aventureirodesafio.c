8cab23b85e21849b8df0126bd619764e040e7ed2
programacaoCS <cristiano_saboya@hotmail.com>
2025-05-18 20:18:13 +0000

    Implementação de Movimento do Bispo

--- 02AVENTUREIRO/aventureirodesafio.c
+++ 02AVENTUREIRO/aventureirodesafio.c
@@ -11,3 +11,13 @@ int main() {
     for (int i = 0; i < torreMovimento; i++) {
          printf("Direita \n");
     }
+
+     // Implementação de Movimento do Bispo 
+
+    printf("\nMovimentação do Bispo: \n");
+    int bispoMovimento = 5;
+    int i = 0;
+    while (i < bispoMovimento) {
+        printf("Cima, Direita \n");
+        i++;
+    }
